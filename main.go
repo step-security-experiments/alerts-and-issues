0232000574153470f240d4cde720d2094013fb71
Raj Kumar Panda <rajkumar@stepsecurity.io>
2024-11-05 10:53:00 +0530

    codeql added

--- main.go
+++ main.go
@@ -1,29 +1,7 @@
 package main
 
-import (
-	"database/sql"
-	"fmt"
-	"os"
-
-	_ "github.com/lib/pq"
-)
+import "os"
 
 func main() {
-	fmt.Println("Starting")
-
-	// Example of a SQL injection vulnerability
-	db, err := sql.Open("postgres", "user=pqgotest dbname=pqgotest sslmode=verify-full")
-	if err != nil {
-		os.Exit(1)
-	}
-	defer db.Close()
-
-	userInput := "1 OR 1=1" // Simulated unsafe user input
-	query := fmt.Sprintf("SELECT * FROM users WHERE id = %s", userInput)
-	_, err = db.Query(query) // This should trigger a CodeQL alert
-	if err != nil {
-		os.Exit(1)
-	}
-
-	os.Exit(1)
+	_, _ = os.Open("example.txt") // This should ideally be flagged
 }
