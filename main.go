de57b471ea6b00130e8de7b4e657a2d908da0717
Raj Kumar Panda <rajkumar@stepsecurity.io>
2024-11-05 11:27:47 +0530

    codeql added

--- main.go
+++ main.go
@@ -1,12 +1,8 @@
 package main
 
 import (
-	"fmt"
-	"os"
+    "net/http"
 )
-
 func main() {
-	file, _ := os.Open("example.txt") // This should raise an alert: ignoring errors
-	defer file.Close()
-	fmt.Println("File opened")
+	
 }
