325d2153df9298b50887721b6394a910dbcd14b2
Raj Kumar Panda <rajkumar@stepsecurity.io>
2024-11-05 10:33:57 +0530

    codeql added

---
+++ go.mod
@@ -0,0 +1,5 @@
+module example.com/m/v2
+
+go 1.23.2
+
+require github.com/lib/pq v1.10.9
--- main.go
+++ main.go
@@ -1,11 +1,29 @@
 package main
 
 import (
+	"database/sql"
 	"fmt"
 	"os"
+
+	_ "github.com/lib/pq"
 )
 
 func main() {
 	fmt.Println("Starting")
-	os.Exit(1) // This line will cause the CodeQL alert
+
+	// Example of a SQL injection vulnerability
+	db, err := sql.Open("postgres", "user=pqgotest dbname=pqgotest sslmode=verify-full")
+	if err != nil {
+		os.Exit(1)
+	}
+	defer db.Close()
+
+	userInput := "1 OR 1=1" // Simulated unsafe user input
+	query := fmt.Sprintf("SELECT * FROM users WHERE id = %s", userInput)
+	_, err = db.Query(query) // This should trigger a CodeQL alert
+	if err != nil {
+		os.Exit(1)
+	}
+
+	os.Exit(1)
 }
